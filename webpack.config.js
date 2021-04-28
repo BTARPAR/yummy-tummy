0bd3cc86296d6db35bb9b4af99e97ca5a1467004
Bhargav Tarpara <bhargav_tarpara@yahoo.co.in>
2021-04-27 22:47:41 -0700

    DEPLOY CHANGES

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
     // plugin omitted
     plugins: [
         new webpack.DefinePlugin({
-            'process.env': {URL: JSON.stringify(process.env.API_KEY || 'http://localhost:4000')}
+            'process.env.URL': JSON.stringify(process.env.API_KEY || 'http://localhost:4000')
         })
     ]
 }
